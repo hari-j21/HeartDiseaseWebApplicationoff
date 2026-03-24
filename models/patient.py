b0d5304bbb55b5c57a4af6d6f8760a2b412bb9e5
Hari <harry70462@gmail.com>
2026-03-24 06:08:58 +0530

    Fix patient history route, update app.py, and finalize working code

--- models/patient.py
+++ models/patient.py
@@ -2,13 +2,16 @@ from database.db import db
 from datetime import datetime
 
 class Patient(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    age = db.Column(db.Float)
-    sex = db.Column(db.Float)
-    cp = db.Column(db.Float)
-    trestbps = db.Column(db.Float)
-    chol = db.Column(db.Float)
-    probability = db.Column(db.Float)
-    confidence = db.Column(db.Float)
+
+    age = db.Column(db.Integer)
+    sex = db.Column(db.Integer)
+    cp = db.Column(db.Integer)
+
+    trestbps = db.Column(db.Integer)
+    chol = db.Column(db.Integer)
+
     prediction = db.Column(db.String(50))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
